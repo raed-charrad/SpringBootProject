32bebe1aaa0d707fd0c814ebcf47ee79e5b922a9
raed charrad <raed.charrad91@gmail.com>
2022-04-07 14:43:32 +0100

    form validation

--- src/main/java/com/example/produits/entities/produit.java
+++ src/main/java/com/example/produits/entities/produit.java
@@ -6,6 +6,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class produit {
@@ -13,9 +23,21 @@ public class produit {
 		@Id
 		@GeneratedValue (strategy = GenerationType.IDENTITY)
 		private Long idProduit;
+		@NotNull
+		@Size (min = 4,max = 15)
 		private String nomProduit;
+		@Min(value = 10)
+		@Max(value = 10000) 
+
 		private Double prixProduit;
+		@Temporal(TemporalType.DATE)
+		@DateTimeFormat(pattern = "yyyy-MM-dd")
+		@PastOrPresent
+
 		private Date dateCreation; 
+		@ManyToOne
+		private Categorie categorie;
+
 		public produit(String nomProduit, Double prixProduit, Date dateCreation) {
 		super();
 		this.nomProduit = nomProduit;
