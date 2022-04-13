65cb9cfb896e7fe66d9c9c855516f6eb00e79d4c
raed charrad <raed.charrad91@gmail.com>
2022-04-13 10:27:44 +0100

    add incription for password

--- src/main/java/com/example/produits/security/SecurityConfig.java
+++ src/main/java/com/example/produits/security/SecurityConfig.java
@@ -1,19 +1,23 @@
 package com.example.produits.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-@Override
-protected void configure(AuthenticationManagerBuilder auth) throws Exception {
- // TODO Auto-generated method stub
-auth.inMemoryAuthentication().withUser("admin").password("{noop}123").roles("ADMIN");
-auth.inMemoryAuthentication().withUser("Najla").password("{noop}123").roles("AGENT","USER");
-auth.inMemoryAuthentication().withUser("user1").password("{noop}123").roles("USER");
- }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+     // TODO Auto-generated method stub
+     PasswordEncoder passwordEncoder = passwordEncoder ();
+     auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("123")).roles("ADMIN");
+     auth.inMemoryAuthentication().withUser("Najla").password(passwordEncoder.encode("123")).roles("AGENT","USER");
+     auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder.encode("123")).roles("USER");
+     }
 @Override
 protected void configure(HttpSecurity http) throws Exception {
  // TODO Auto-generated method stub
@@ -21,8 +25,14 @@ protected void configure(HttpSecurity http) throws Exception {
  http.authorizeRequests().antMatchers("/ListeProduits").hasAnyRole("ADMIN","AGENT","USER");
  http.authorizeRequests().antMatchers("/supprimerProduit","/modifierProduit","/updateProduit").hasAnyRole("ADMIN");
  http.authorizeRequests().anyRequest().authenticated();
+ http.exceptionHandling().accessDeniedPage("/accessDenied"); 
+
  http.formLogin();
 
  }
+ @Bean
+ public PasswordEncoder passwordEncoder () {
+ return new BCryptPasswordEncoder();
+ } 
 
 } 
