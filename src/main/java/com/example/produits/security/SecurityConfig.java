df80ead6baf0a31551cfb59d333e0345fb48d94e
raed charrad <raed.charrad91@gmail.com>
2022-04-13 09:32:13 +0100

    add acces security

--- src/main/java/com/example/produits/security/SecurityConfig.java
+++ src/main/java/com/example/produits/security/SecurityConfig.java
@@ -17,8 +17,12 @@ auth.inMemoryAuthentication().withUser("user1").password("{noop}123").roles("USE
 @Override
 protected void configure(HttpSecurity http) throws Exception {
  // TODO Auto-generated method stub
+ http.authorizeRequests().antMatchers("/showCreate","/saveProduit").hasAnyRole("ADMIN","AGENT");
+ http.authorizeRequests().antMatchers("/ListeProduits").hasAnyRole("ADMIN","AGENT","USER");
+ http.authorizeRequests().antMatchers("/supprimerProduit","/modifierProduit","/updateProduit").hasAnyRole("ADMIN");
  http.authorizeRequests().anyRequest().authenticated();
  http.formLogin();
+
  }
 
 } 
