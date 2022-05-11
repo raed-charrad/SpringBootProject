b6712516e49bebba60fe8fa276cec37dc8c0f856
raed charrad <raed.charrad91@gmail.com>
2022-05-11 12:07:12 +0100

    add login and logout to controller

--- src/main/java/com/example/produits/security/SecurityController.java
+++ src/main/java/com/example/produits/security/SecurityController.java
@@ -1,17 +1,31 @@
 package com.example.produits.security;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SecurityController {
-@GetMapping("/accessDenied")
-    public String geterror() {
-        return "accessDenied";
-    }
+	@GetMapping("/accessDenied")
+	    public String geterror() {
+	        return "accessDenied";
+	    }
     @PostMapping("/accessDenied")
-    public String posterror() {
-        return "accessDenied";
-    }
+	    public String posterror() {
+	        return "accessDenied";
+	    }
+    @GetMapping("/login")
+	    public String login()
+	    {
+	    return "login";
+	    } 
+    @GetMapping("/logout")
+	    public String logout(HttpServletRequest request) throws ServletException
+	    {
+	    request.logout();
+	    return "redirect:/login";
+	    }
 } 
