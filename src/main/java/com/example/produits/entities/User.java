c669c664c86af87b8f0ad52d12a53d9e98177389
raed charrad <raed.charrad91@gmail.com>
2022-05-11 12:04:50 +0100

    create user entity

---
+++ src/main/java/com/example/produits/entities/User.java
@@ -0,0 +1,80 @@
+package com.example.produits.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.*;
+
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "USER")
+public class User implements Serializable, UserDetails {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer userId;
+	private String username;
+	private String password;
+	@ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+	@JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"),
+	inverseJoinColumns = @JoinColumn(name = "role_id"))
+
+	private Set<Role> roles = new HashSet<>();
+	public Integer getUserId() {
+	 return userId;
+	 }
+	public void setUserId(Integer userId) {
+	 this.userId = userId;
+	 }
+	public String getUsername() {
+	 return username;
+	 }
+	@Override
+	public boolean isAccountNonExpired() {
+	 return false;
+	 }
+	@Override
+	public boolean isAccountNonLocked() {
+	 return false;
+	 }
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+		}
+	@Override
+	public boolean isEnabled() {
+		return false;
+		}
+	public void setUsername(String username) {
+		this.username = username;
+		}
+	
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+	@Override
+	public Collection <? extends GrantedAuthority> getAuthorities() {
+		Set<Role> roles = this.getRoles();
+		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+		for (Role role : roles) {
+		authorities.add(new SimpleGrantedAuthority(role.getName()));
+		} return authorities;
+		}
+	public String getPassword() {
+		return password;
+		}
+	public void setPassword(String password) {
+		this.password = password;
+		} 
+
+}
