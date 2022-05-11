82b8fd5cfec5b231617f32311fc2897e3d866632
raed charrad <raed.charrad91@gmail.com>
2022-05-11 12:06:30 +0100

    add authorization to security config

--- src/main/java/com/example/produits/security/SecurityConfig.java
+++ src/main/java/com/example/produits/security/SecurityConfig.java
@@ -1,38 +1,102 @@
 package com.example.produits.security;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.access.AccessDeniedHandlerImpl;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 
+import com.example.produits.service.UserService;
+
+
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-     // TODO Auto-generated method stub
-     PasswordEncoder passwordEncoder = passwordEncoder ();
-     auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("123")).roles("ADMIN");
-     auth.inMemoryAuthentication().withUser("Najla").password(passwordEncoder.encode("123")).roles("AGENT","USER");
-     auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder.encode("123")).roles("USER");
-     }
-@Override
-protected void configure(HttpSecurity http) throws Exception {
- // TODO Auto-generated method stub
- http.authorizeRequests().antMatchers("/showCreate","/saveProduit").hasAnyRole("ADMIN","AGENT");
- http.authorizeRequests().antMatchers("/ListeProduits").hasAnyRole("ADMIN","AGENT","USER");
- http.authorizeRequests().antMatchers("/supprimerProduit","/modifierProduit","/updateProduit").hasAnyRole("ADMIN");
- http.authorizeRequests().anyRequest().authenticated();
- http.exceptionHandling().accessDeniedPage("/accessDenied"); 
+	public static final String AUTHORITIES_CLAIM_NAME = "roles";
+	@Autowired
+	 UserService userDetailsService;
+	@Autowired
+	private AccessDeniedHandler accessDeniedHandler;  
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+		.authenticationProvider(getProvider())
+		.formLogin().loginPage("/login")
+		.loginProcessingUrl("/login")
+		.successHandler(new AuthentificationLoginSuccessHandler())
+		.failureHandler(new SimpleUrlAuthenticationFailureHandler())
+		.and()
+		.logout()
+		.logoutUrl("/logout")
+		.logoutSuccessHandler(new AuthentificationLogoutSuccessHandler())
+		.invalidateHttpSession(true)
+		.and()
+		.authorizeRequests()
+		.antMatchers("/login").permitAll()
+		.antMatchers("/register").permitAll()
+		.antMatchers("/logout").permitAll()
+		.antMatchers("/webjars/**").permitAll(); 
+//		.anyRequest().authenticated();
 
- http.formLogin();
+	http.authorizeRequests().antMatchers("/showCreate","/saveProduit").hasAnyRole("ADMIN","AGENT");
+	 http.authorizeRequests().antMatchers("/ListeProduits").hasAnyRole("ADMIN","AGENT","USER");
+	 http.authorizeRequests().antMatchers("/supprimerProduit","/modifierProduit","/updateProduit").hasAnyRole("ADMIN");
+	 http.authorizeRequests().anyRequest().authenticated();
+	 http.exceptionHandling().accessDeniedPage("/accessDenied"); 
+	
+	 http.formLogin();
+	
+	 }
+	
+		
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	 auth.userDetailsService(userDetailsService);
+	 }
+	private class AuthentificationLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+		@Override
+		public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+		Authentication authentication) throws IOException, ServletException {
+			response.setStatus(HttpServletResponse.SC_OK);
+		 }
+	 }
+	private class AuthentificationLogoutSuccessHandler extends SimpleUrlLogoutSuccessHandler {
+		@Override
+		public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, 
+				Authentication authentication) throws IOException, ServletException {
+		response.setStatus(HttpServletResponse.SC_OK);
+		 }
+	 }
+	@Bean
+	public AuthenticationProvider getProvider() {
+	 AppAuthProvider provider = new AppAuthProvider();
+	provider.setUserDetailsService(userDetailsService);
+	return provider;
+	 }
+	 @Bean
+	 public PasswordEncoder passwordEncoder () {
+	 return new BCryptPasswordEncoder();
+	 } 
+	@Bean
+	public AccessDeniedHandler accessDeniedHandler(){
+	return new AccessDeniedHandlerImpl();
+	}
+	}
 
- }
- @Bean
- public PasswordEncoder passwordEncoder () {
- return new BCryptPasswordEncoder();
- } 
-
-} 
